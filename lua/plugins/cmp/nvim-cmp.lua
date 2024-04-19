16c9bc7a0c3dfa03734a8202443068fb3f2c7db2
push edp <aspushedp@gmail.com>
2024-04-19 15:33:40 +1000

    update

--- lua/plugins/cmp/nvim-cmp.lua
+++ lua/plugins/cmp/nvim-cmp.lua
@@ -2,23 +2,27 @@ return {
     "hrsh7th/nvim-cmp",
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-nvim-lua",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
         "saadparwaiz1/cmp_luasnip",
         "onsails/lspkind.nvim",
+        "L3MON4D3/LuaSnip",
+        "rafamadriz/friendly-snippets",
     },
     event = "VeryLazy",
     config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+
         local cmp = require("cmp")
         local luasnip = require("luasnip")
         local lspkind = require("lspkind")
 
         local source_menu = {
-            nvim_lsp = "[lsp]",
-            nvim_lua = "[nvim]",
-            luasnip  = "[snippet]",
+            nvim_lsp = "[LSP]",
+            luasnip  = "[SNIP]",
+            path     = "[PATH]",
+            buffer   = "[BUFFER]",
         }
 
         cmp.setup({
@@ -39,15 +43,14 @@ return {
                 { name = "luasnip" },
                 { name = "buffer" },
                 { name = "path" },
-                { name = "cmdline" },
             }),
             mapping = cmp.mapping.preset.insert({
                 ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
                 ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
                 ["<C-e>"] = cmp.mapping.abort(),
+                ["<C-Space>"] = cmp.mapping.complete(),
                 ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
                 ["<S-CR>"] = cmp.mapping.confirm({
                     behavior = cmp.ConfirmBehavior.Replace,
@@ -84,11 +87,6 @@ return {
             },
         })
 
-        cmp.setup.filetype('lua', {
-            sources = cmp.config.sources({
-                { name = 'nvim_lua' }
-            })
-        })
         cmp.setup.cmdline({ '/', '?' }, {
             mapping = cmp.mapping.preset.cmdline(),
             sources = {
