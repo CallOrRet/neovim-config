74441dd3246229eab102453e27b1c1fa0d191b98
push edp <aspushedp@gmail.com>
2024-04-19 15:37:52 +1000

    update

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -14,7 +14,9 @@ return {
         capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
         require("lspconfig.ui.windows").default_options.border = "rounded"
+
         vim.diagnostic.config({ float = { border = 'rounded' } })
+
         vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
         vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help,
             { border = "rounded" })
