113cf75f2a8aacce68bc4a93c0fb94c592804415
push edp <aspushedp@gmail.com>
2024-04-20 06:02:29 +1000

    update

--- lua/plugins/ui/theme.lua
+++ lua/plugins/ui/theme.lua
@@ -1,7 +1,7 @@
 return {
     {
         "lunarvim/onedarker.nvim",
-        lazy = false,
+        lazy = true,
         priority = 1000,
         opts = {},
     },
@@ -12,10 +12,10 @@ return {
         opts = { sytle = "warmer" }, -- Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
     },
     {
+
         "bluz71/vim-moonfly-colors",
         name = "moonfly",
-        lazy = true,
-        -- lazy = false, -- load this default theme
+        lazy = false, -- load this default theme
         priority = 1000,
         config = function()
             vim.g.moonflyWinSeparator = 2
