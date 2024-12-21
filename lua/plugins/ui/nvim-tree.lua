43854717ebeee0797aaaa073c37898c478ab8fde
CallOrRet <CallOrRet@outlook.com>
2024-12-22 06:41:34 +1000

    update

--- lua/plugins/ui/nvim-tree.lua
+++ lua/plugins/ui/nvim-tree.lua
@@ -53,10 +53,6 @@ return {
             },
             sync_root_with_cwd = true,
             respect_buf_cwd = true,
-            update_focused_file = {
-                enable = true,
-                update_root = true
-            },
         })
     end,
 }
